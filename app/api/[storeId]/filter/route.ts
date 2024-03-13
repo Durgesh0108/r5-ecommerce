7a4542802522afd712cae1c048a8464ba9ea61e7
Durgesh Prajapati <drgaming143@gmail.com>
2024-03-13 12:26:36 +0530

    changed the products filter method from id to name of the category and added search funcationality through api

--- app/api/[storeId]/filter/route.ts
+++ app/api/[storeId]/filter/route.ts
@@ -7,10 +7,6 @@ export async function GET(
 ) {
   const { searchParams } = new URL(req.url)
   const query = searchParams.get('q') || undefined
-  const categoryId = searchParams.get('categoryId') || undefined;
-  const colorId = searchParams.get('colorId') || undefined;
-  const sizeId = searchParams.get('sizeId') || undefined;
-  const isFeatured = searchParams.get('isFeatured');
 
   if (!params.storeId) {
     return new NextResponse("Store id is required", { status: 400 });
--- app/api/[storeId]/products/route.ts
+++ app/api/[storeId]/products/route.ts
@@ -86,25 +86,44 @@ export async function POST(
 
 export async function GET(
   req: Request,
-  { params }: { params: { storeId: string } },
+  { params }: { params: { storeId: string, categoryId: string } },
 ) {
   try {
     const { searchParams } = new URL(req.url)
+    // const categoryId = searchParams.get('categoryId') || undefined;
     const categoryId = searchParams.get('categoryId') || undefined;
-    const colorId = searchParams.get('colorId') || undefined;
-    const sizeId = searchParams.get('sizeId') || undefined;
+    const color = searchParams.get('color') || undefined;
+    const size = searchParams.get('size') || undefined;
     const isFeatured = searchParams.get('isFeatured');
 
     if (!params.storeId) {
       return new NextResponse("Store id is required", { status: 400 });
     }
 
+    console.log("categoryId",categoryId)
+    console.log("Size",size)
+    console.log("Color",color)
+
     const products = await prismadb.product.findMany({
       where: {
         storeId: params.storeId,
         categoryId,
-        colorId,
-        sizeId,
+        // category: {
+        //   name: {
+        //     equals: category
+        //   }
+        // },
+        // categoryId,
+        color: {
+          name: {
+            contains: color
+          }
+        },
+        size: {
+          name: {
+            contains: size
+          }
+        },
         isFeatured: isFeatured ? true : undefined,
         isArchived: false,
       },
@@ -125,3 +144,109 @@ export async function GET(
     return new NextResponse("Internal error", { status: 500 });
   }
 };
+
+// export async function GET(
+//   req: Request,
+//   { params }: { params: { storeId: string } },
+// ) {
+
+//   const { searchParams } = new URL(req.url)
+//   const categoryId = searchParams.get('categoryId') || undefined;
+//   const color = searchParams.get('color') || undefined;
+//   const size = searchParams.get('size') || undefined;
+//   const isFeatured = searchParams.get('isFeatured');
+
+//   if (!params.storeId) {
+//     return new NextResponse("Store id is required", { status: 400 });
+//   }
+
+//   let filterProducts: {}[] = []
+
+//   const categoryProducts = await prismadb.product.findMany({
+//     where: {
+//       storeId: params.storeId,
+//       categoryId,
+//       // isFeatured: isFeatured ? true : undefined,
+//       // isArchived: false,
+//     },
+//     include: {
+//       images: true,
+//       // category: true,
+//       // color: true,
+//       // size: true,
+//     },
+//     orderBy: {
+//       createdAt: 'desc',
+//     }
+//   });
+//   console.log("category", categoryProducts)
+
+//   const sizeProducts = await prismadb.product.findMany({
+//     where: {
+//       storeId: params.storeId,
+//       categoryId,
+//       size: {
+//         name: size
+//       },
+//       // isFeatured: isFeatured ? true : undefined,
+//       // isArchived: false,
+//     },
+//     include: {
+//       images: true,
+//       // category: true,
+//       // color: true,
+//       // size: true,
+//     },
+//     orderBy: {
+//       createdAt: 'desc',
+//     }
+//   });
+//   console.log("size", sizeProducts)
+//   const colorProducts = await prismadb.product.findMany({
+//     where: {
+//       storeId: params.storeId,
+//       categoryId,
+//       color: {
+//         name: color
+//       },
+//       // isFeatured: isFeatured ? true : undefined,
+//       // isArchived: false,
+//     },
+//     include: {
+//       images: true,
+//       // category: true,
+//       // color: true,
+//       // size: true,
+//     },
+//     orderBy: {
+//       createdAt: 'desc',
+//     }
+//   });
+//   console.log("color", colorProducts)
+
+//   filterProducts = [{ category: { ...categoryProducts }, color: { ...colorProducts }, size: { ...sizeProducts } }]
+
+//   function filterDuplicateProducts(products: {}[]) {
+//     const uniqueProducts = [];
+//     const encounteredIds = new Set();
+
+//     for (const product of products) {
+//       // @ts-ignore
+//       if (!encounteredIds.has(product.id)) {
+//         uniqueProducts.push(product);
+//         // @ts-ignore
+//         encounteredIds.add(product.id);
+//       }
+//     }
+
+//     return uniqueProducts;
+//   }
+//   try {
+//     // console.log("filter", filterProducts)
+//     // const products = await filterDuplicateProducts(filterProducts)
+//     return NextResponse.json(filterProducts);
+//   } catch (error) {
+//     console.log('[PRODUCTS_GET]', error);
+//     return new NextResponse("Internal error", { status: 500 });
+//   }
+// };
